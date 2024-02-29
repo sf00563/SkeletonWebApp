f57b7e8dfe6519fd8003cce8d967ac53e6e5621b
Sam Foster <samf2363@icloud.com>
2024-02-29 12:26:25 +0000

    change app title

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
   @Override
   protected void writeContentTo(PrintWriter writer) {
     writer.println(
-        "<h1>Welcome!</h1>"
+        "<h1>Welcome to app-training-128!</h1>"
             + "<p>Please enter your term in the box below: "
             + "<form><input type=\"text\" name=\"q\" />"
             + "<input type=\"submit\">"
