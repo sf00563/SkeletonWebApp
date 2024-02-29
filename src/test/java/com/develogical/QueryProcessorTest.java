d73e0b2896e6ef20f7dd9cb620eab8bb32c091ba
Sam Foster <samf2363@icloud.com>
2024-02-29 12:01:50 +0000

    add test for golf query

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -20,4 +20,9 @@ public class QueryProcessorTest {
     assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
 
+  @Test
+  public void knowsAboutGolf() throws Exception {
+    assertThat(queryProcessor.process("Golf"), containsString("golf is fun"));
+  }
+
 }
